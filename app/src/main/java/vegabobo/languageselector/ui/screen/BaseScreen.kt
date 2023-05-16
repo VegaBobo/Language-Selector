1745f9e975678a07f09b26bc39ab11f6bbef89a7
VegaBobo <vegazstestando@gmail.com>
2023-05-15 23:23:31 -0300

    Specify scroll behavior TopAppBar

--- app/src/main/java/vegabobo/languageselector/ui/screen/BaseScreen.kt
+++ app/src/main/java/vegabobo/languageselector/ui/screen/BaseScreen.kt
@@ -3,15 +3,15 @@ package vegabobo.languageselector.ui.screen
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,20 +21,18 @@ fun BaseScreen(
     actions: @Composable RowScope.() -> Unit = {},
     content: @Composable (PaddingValues) -> Unit,
 ) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Scaffold(
-            modifier = Modifier.statusBarsPadding(),
-            topBar = {
-                TopAppBar(
-                    navigationIcon = { navIcon() },
-                    title = { Text(screenTitle) },
-                    actions = { actions(this) }
-                )
-            },
-            content = { content(it) }
-        )
-    }
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                scrollBehavior = scrollBehavior,
+                navigationIcon = { navIcon() },
+                title = { Text(screenTitle) },
+                actions = { actions(this) }
+            )
+        },
+        content = { content(it) }
+    )
 }
